319329bb53608deee01b6ca2e44b90213b41fa13
Zheng Chaoping <zbinlin@gmail.com>
2015-11-18 23:57:51 +0800

    fixes some bugs

--- examples/app.js
+++ examples/app.js
@@ -17,6 +17,9 @@ export default class App extends Component {
         };
     }
     render() {
+        let icon = <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAC4AAAAuCAMAAABgZ9sFAAAAVFBMVEXx8fHMzMzr6+vn5+fv7+/t7e3d3d2+vr7W1tbHx8eysrKdnZ3p6enk5OTR0dG7u7u3t7ejo6PY2Njh4eHf39/T09PExMSvr6+goKCqqqqnp6e4uLgcLY/OAAAAnklEQVRIx+3RSRLDIAxE0QYhAbGZPNu5/z0zrXHiqiz5W72FqhqtVuuXAl3iOV7iPV/iSsAqZa9BS7YOmMXnNNX4TWGxRMn3R6SxRNgy0bzXOW8EBO8SAClsPdB3psqlvG+Lw7ONXg/pTld52BjgSSkA3PV2OOemjIDcZQWgVvONw60q7sIpR38EnHPSMDQ4MjDjLPozhAkGrVbr/z0ANjAF4AcbXmYAAAAASUVORK5CYII="
+                        alt="icon"
+                        style={{width: "20px", marginRight: "5px", display: "block"}} />
         return (
             <div>
                 <Button>按钮</Button>
@@ -101,18 +104,18 @@ export default class App extends Component {
                 </div>
                 <hr />
                 <div>
+                    <Cells title={<h3 style={{margin: 0}}>Heading</h3>}>
+                        <Cell>Hello World</Cell>
+                    </Cells>
                     <Cells>
                         <Cell>Hello World</Cell>
                     </Cells>
                     <Cells title={<h3 style={{margin: 0}}>Heading</h3>}>
-                        <Cell>Hello World</Cell>
-                    </Cells>
-                    <Cells title={<h3 style={{margin: 0}}>Heading</h3>}>
-                        <Cell header={<Icon type="info" />}>Hello World</Cell>
+                        <Cell header={icon}>Hello World</Cell>
                     </Cells>
                     <Cells title={<h3 style={{margin: 0}}>Heading</h3>}>
                         <Cell footer={<p style={{margin: 0}}>Description</p>}>Hello World</Cell>
-                        <Cell href="">Hello World</Cell>
+                        <Cell href="javascript:">Hello World</Cell>
                     </Cells>
                 </div>
             </div>
