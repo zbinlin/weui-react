30b9ae1270a191dce04d01441ac66fb4cc5f5465
Zheng Chaoping <zbinlin@gmail.com>
2015-12-14 22:05:14 +0800

    Improve Button, ButtonGroup Component

--- src/components/button/group.jsx
+++ src/components/button/group.jsx
@@ -9,8 +9,14 @@ const cx = classNames.bind(styles);
 
 export default class ButtonGroup extends Component {
     render() {
+        let props = this.props;
+        let style = props.style;
+        let classList = ["root"];
+        if (props.align === "justify") {
+            classList.push("justify");
+        }
         return (
-            <div className={cx("group")} role="group">
+            <div className={cx(...classList)} role="group" style={style}>
                 {this.props.children}
             </div>
         );
